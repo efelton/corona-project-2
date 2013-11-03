046bc58b0ce3cb8adc908badc4164f4bdc81660d
efelton <eoghan.felton@gmail.com>
2013-11-03 20:24:41 +0000

    Added betters comments to scene3.lua; removed boilerplate

--- scene3.lua
+++ scene3.lua
@@ -9,39 +9,19 @@ local scene = storyboard.newScene()
 
 require("navbar")
 
-----------------------------------------------------------------------------------
--- 
---	NOTE:
---	
---	Code outside of listener functions (below) will only be executed once,
---	unless storyboard.removeScene() is called.
--- 
----------------------------------------------------------------------------------
-
----------------------------------------------------------------------------------
--- BEGINNING OF YOUR IMPLEMENTATION
----------------------------------------------------------------------------------
+-- function for the navbar to change the scene to the one selected
 function nextScene(event)
 	if event.phase == "ended" then
 		storyboard.gotoScene( event.target.myname )
 	end
 end
 
-
-
-
 -- Called when the scene's view does not exist:
 function scene:createScene( event )
 	local group = self.view
 
-	-----------------------------------------------------------------------------
-		
-	--	CREATE display objects and add them to 'group' here.
-	--	Example use-case: Restore 'group' from previously saved state.
-	
-	-----------------------------------------------------------------------------
+	-- set up the navbar
 	setupNavbar(group)
-	
 end
 
 
@@ -51,6 +31,7 @@ function scene:enterScene( event )
 	
 	local params = event.params
 
+	-- set up the imageHeader, message and longMessage
 	imageHeader = display.newImage(params.var3)
 	imageHeader.x = display.contentWidth *0.5
 	imageHeader.y = display.contentHeight * 0.3
@@ -65,38 +46,32 @@ function scene:enterScene( event )
 	longMessage.x = display.contentWidth/2
 	group:insert(longMessage)
 
+	-- set up the event listeners for the navbar
 	setupNavbarHandlers(nextScene)
- 
-
-	-----------------------------------------------------------------------------
-	
 end
 
 
 -- Called when scene is about to move offscreen:
 function scene:exitScene( event )
 	local group = self.view
-	
+
+	-- remove up the event listeners for the navbar
 	removeNavbarHandlers(nextScene)
+
+	-- delete the details for the selected item to make room for the next one selected
 	imageHeader:removeSelf() 
 	message:removeSelf()
 	longMessage:removeSelf()
 
+	-- a problem with the navbar means that if the user returns to a screen with the navbar
+	-- the buttons don't work; calling storyboard.removeall() works around this issue	
 	storyboard.removeAll()
-
 end
 
 
 -- Called prior to the removal of scene's "view" (display group)
 function scene:destroyScene( event )
 	local group = self.view
-	
-	-----------------------------------------------------------------------------
-	
-	--	INSERT code here (e.g. remove listeners, widgets, save state, etc.)
-	
-	-----------------------------------------------------------------------------
-	
 end
 
 
